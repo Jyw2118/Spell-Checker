eb2ed7881828782ee6790dcabdb36ecc0174349d
Joanne Wang <JoanneWang@Joannes-MacBook-Pro-5.local>
2020-07-22 17:18:39 -0400

    Create SpellCheckerInterface.java

---
+++ SpellCheckerInterface.java
@@ -0,0 +1,9 @@
+//Joanne Wang
+
+import java.util.*;
+
+public interface SpellCheckerInterface
+{
+	public List<String> getIncorrectWords(String filename);
+	public Set<String> getSuggestions(String word);
+}
